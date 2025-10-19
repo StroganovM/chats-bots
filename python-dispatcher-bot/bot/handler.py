35804dcd0b929b5bdecd3ec89cd57e8a2514dcda
Mikhail Andreevich <mikhail_stroganov@bk.ru>
2025-10-19 23:35:33 +0300

    add LR 2

---
+++ python-dispatcher-bot/bot/handler.py
@@ -0,0 +1,9 @@
+from abc import ABC, abstractmethod
+
+
+class Handler(ABC):
+    @abstractmethod
+    def can_handle(self, update: dict) -> bool: ...
+
+    @abstractmethod
+    def handle(self, update: dict) -> bool: ...
